6953235300883255591774b173a3e5a97b3c0a86
lokakos01 <114986974+lokakos01@users.noreply.github.com>
2023-06-22 14:31:31 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,51 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+
+ 
+
+const app = express();
+app.use(express.json());
+
+ 
+
 app.post('/add', (req, res) => {
-
-    console.log("Mottagen");
-
- 
-
-    const namn = req.body.namn;
-
-    const kategori = req.body.kategori;
-
-    const kommentar = req.body.kommentar;
+  console.log("Mottagen");
+  const namn = req.body.namn;
+  const kategori = req.body.kategori;
+  const kommentar = req.body.kommentar;
 
  
 
-    fs.readFile('data.json', (err, data) => {
+  fs.readFile('data.json', (err, data) => {
+    if (err) throw err;
+    const comments = JSON.parse(data);
 
+ 
+
+    const newComment = {
+      namn: namn,
+      kategori: kategori,
+      kommentar: kommentar,
+    };
+
+ 
+
+    comments.push(newComment);
+
+ 
+
+    fs.writeFile('data.json', JSON.stringify(comments), (err) => {
       if (err) throw err;
-
-      const comments = JSON.parse(data);
-
- 
-
-      const newComment = {
-
-        namn: namn,
-
-        kategori: kategori,
-
-        kommentar: kommentar,
-
-      };
-
- 
-
-      comments.push(newComment);
-
- 
-
-      fs.writeFile('data.json', JSON.stringify(comments), (err) => {
-
-        if (err) throw err;
-
-        console.log('Comment Confirmed');
-
-        res.status(200).json(newComment); // Returnera JSON-respons istället för en sträng
-
-      });
-
+      console.log('Comment Confirmed');
+      res.status(200).json(newComment);
     });
-
   });
+});
+
+ 
+
+app.listen(3000, () => {
+  console.log('Server lyssnar på port 3000');
+});
